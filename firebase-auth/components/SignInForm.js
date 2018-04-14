849510f8037928b15ccc3dd499b5a73e1a51457b
ALTRAN_ES\alberto.quiros <alberto.munoz@altran.com>
2018-04-14 16:35:20 +0200

    Created Sign in form to call post cloud function to verify code

---
+++ firebase-auth/components/SignInForm.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import { FormLabel, FormInput, Button } from 'react-native-elements';
+import axios from 'axios';
+
+const ROOT_URL = 'https://us-central1-one-time-password-amqo.cloudfunctions.net';
+
+class SignInForm extends Component {
+
+  state = { phone: '', code: '' };
+
+  handleSubmit = async () => {
+    const { phone, code } = this.state;
+    try {
+      await axios.post(`${ROOT_URL}/verifyOneTimePassword`, { phone, code })
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  render() {
+    return (
+      <View>
+        <View style={{ marginBottom: 10 }}>
+          <FormLabel>Enter Phone Number</FormLabel>
+          <FormInput
+            value={ this.state.phone }
+            onChangeText={phone => this.setState({ phone })}
+          />
+        </View>
+        <View style={{ marginBottom: 10 }}>
+          <FormLabel>Enter Code Received</FormLabel>
+          <FormInput
+            value={ this.state.code }
+            onChangeText={code => this.setState({ code })}
+          />
+        </View>
+        <Button onPress={this.handleSubmit} title="Submit" />
+      </View>
+    );
+  }
+}
+
+export default SignInForm;

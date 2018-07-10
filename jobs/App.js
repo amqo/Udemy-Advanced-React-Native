69aad1b46b2f3b542c3fd2b0d16da60d9d0ea209
ALTRAN_ES\alberto.quiros <alberto.munoz@altran.com>
2018-07-10 18:32:30 +0200

    Added empty screens and main navigation to new Jobs project.

--- jobs/App.js
+++ jobs/App.js
@@ -1,11 +1,44 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Platform, StyleSheet, Text, View } from 'react-native';
+import {
+  createBottomTabNavigator,
+  createStackNavigator
+} from "react-navigation";
+import { Constants } from "expo";
+
+import AuthScreen from './screens/AuthScreen';
+import WelcomeScreen from './screens/WelcomeScreen';
+import MapScreen from './screens/MapScreen';
+import DeckScreen from './screens/DeckScreen';
+import ReviewScreen from './screens/ReviewScreen';
+import SettingsScreen from './screens/SettingsScreen';
 
 export default class App extends React.Component {
   render() {
+
+    const ReviewNavigator = createStackNavigator({
+      review: ReviewScreen,
+      settings: SettingsScreen
+    },
+    {
+      navigationOptions: {
+        headerStyle: styles.stackHeaderStyle
+      }
+    });
+
+    const MainNavigator = createBottomTabNavigator({
+      welcome: WelcomeScreen,
+      auth: AuthScreen,
+      main: createBottomTabNavigator({
+        map: MapScreen,
+        deck: DeckScreen,
+        review: ReviewNavigator
+      })
+    });
+
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <MainNavigator />
       </View>
     );
   }
@@ -14,8 +47,17 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    ...Platform.select({
+      android: {
+        marginTop: Constants.statusBarHeight
+      }
+    })
   },
+  stackHeaderStyle: {
+    ...Platform.select({
+      android: {
+        marginTop: -Constants.statusBarHeight
+      }
+    })
+  }
 });
